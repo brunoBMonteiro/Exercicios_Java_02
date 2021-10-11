d4f0444a2a57aeea42548fb4ec2a1b477f3bd658
brunoBMonteiro <brunobilheri@hotmail.com>
2021-10-11 11:21:23 -0300

    #013 Day of the week

---
+++ ex013/src/DiaSemana.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DiaSemana {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite um numero: ");
+        int diaDaSemana = input.nextInt();
+
+        switch (diaDaSemana){
+            case 1: System.out.println("Domingo"); break;
+            case 2: System.out.println("Segunda"); break;
+            case 3: System.out.println("Terça"); break;
+            case 4: System.out.println("Quarta"); break;
+            case 5: System.out.println("Quinta"); break;
+            case 6: System.out.println("Sexta"); break;
+            case 7: System.out.println("Sábado"); break;
+            default: System.out.println("Dia inválido");
+        }
+    }
+}
