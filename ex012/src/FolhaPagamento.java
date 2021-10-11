4158f79aafc149efef072b5a4ca4b9e5124101bf
brunoBMonteiro <brunobilheri@hotmail.com>
2021-10-11 11:05:57 -0300

    #012 Payroll discounts

--- ex012/src/FolhaPagamento.java
+++ ex012/src/FolhaPagamento.java
@@ -9,31 +9,32 @@ public class FolhaPagamento {
         System.out.println("Digite a quantidade de horas trabalhadas mês:");
         double qtdHorasTrabalhadaMes = input.nextDouble();
 
-        double salarioLiquido = valorHoraTrabalhada * qtdHorasTrabalhadaMes;
-        double descontoIR = 5;
-        double descontoINSS = 10;
-        double descontoFGTS = 11;
-        double descontos = descontoIR + descontoINSS + descontoFGTS;
-        double salarioBruto = salarioLiquido - descontos;
+        double salarioBruto = valorHoraTrabalhada * qtdHorasTrabalhadaMes;
+        double descontoIR = 0;
 
-        if(salarioBruto < 900){
-            salarioBruto = salarioBruto;
-        } else if (salarioBruto < 1500){
-            double valor1 = 5;
-            salarioBruto -= (valor1 * salarioBruto) / 100;
-            System.out.println(salarioBruto);
-        } else if (salarioBruto < 2500){
-            double valor2 = 10;
-            salarioBruto -= (valor2 * salarioBruto) / 100;
-            System.out.println(salarioBruto);
+
+        if(salarioBruto <= 900){
+            descontoIR = 0;
+        } else if (salarioBruto > 900 && salarioBruto <= 1500){
+            descontoIR = 5;
+        } else if (salarioBruto > 1500 && salarioBruto < 2500){
+            descontoIR = 10;
         } else if (salarioBruto >= 2500){
-            double valor2 = 20;
-            salarioBruto -= (valor2 * salarioBruto) / 100;
-            System.out.println(salarioBruto);
+            descontoIR = 20;
         }
 
-        System.out.println("O salario Liquido é :" + salarioLiquido);
-        System.out.println("O total de Descontos é :" + descontos);
-        System.out.println("O salario Bruto é :" + salarioBruto);
+        double ir = (salarioBruto / 100) * descontoIR;
+        double inss = (salarioBruto / 100) * 10;
+        double fgts = (salarioBruto / 100) * 11;
+        double totalDescontos = ir + inss;
+        double salarioLiquido = salarioBruto - totalDescontos;
+
+
+        System.out.println("O valor hora trabalhada :" + valorHoraTrabalhada + " , " + "A quantidade horas trabalhadas :" + qtdHorasTrabalhadaMes + " , " + "O salario bruto :" + salarioBruto );
+        System.out.println("O valor do IR é :" + descontoIR + ", " + ir );
+        System.out.println("O valor do INSS (10%) :" + inss);
+        System.out.println("O valor do FGTS (11%) é :" + fgts);
+        System.out.println("O total de desconto é :" + totalDescontos);
+        System.out.println("O valor do IR é :" + salarioLiquido);
     }
 }
